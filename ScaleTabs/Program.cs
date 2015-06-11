a20ae401d10dc84064b9e15d7d0b5b174a7c5e44
Jordan Earls <earlz@earlz.net>
2015-06-10 22:00:29 -0400

    making it actually do stuff and getting closer

--- ScaleTabs/Program.cs
+++ ScaleTabs/Program.cs
@@ -92,12 +92,68 @@ namespace ScaleTabs
             ni+=2;
             return (Note)(ni % 12);
         }
-
-
-        void Main(string[] args)
+        static void Main(string[] args)
+        {
+            var m = new Program();
+            m.Stuff(args);
+        }
+        void Stuff(string[] args)
         {
             int fretmax = 22;
+            List<List<Note>> Strings = new List<List<Note>>();
+            Strings.Add(GenerateString(Note.E, fretmax));
+            Strings.Add(GenerateString(Note.B, fretmax));
+            Strings.Add(GenerateString(Note.G, fretmax));
+            Strings.Add(GenerateString(Note.D, fretmax));
+            Strings.Add(GenerateString(Note.A, fretmax));
+            Strings.Add(GenerateString(Note.E, fretmax));
+
+            var l=MarkFrets(Strings[0], Note.C, MajorScale);
+            ;
 
         }
+        void PrintFrets(List<Note> str, List<int> marks, char notation)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(var mark in marks)
+            {
+                sb.Append("|");
+                if(mark == -1)
+                {
+                    sb.Append("--");
+                }
+                else
+                {
+                    sb.Append(notation);
+                    sb.Append(str);
+                }
+                sb.Append("|");
+            }
+        }
+        //returns list with the index corresponding to fret number and value being the "th" of the scale (or -1 for not in it)
+        List<int> MarkFrets(List<Note> str, Note root, Func<Note, Note, int> scale)
+        {
+            var list=new List<int>();
+            for(int i=0;i<str.Count;i++)
+            {
+                list.Add(scale(root, str[i]));
+            }
+            return list;
+        }
+        List<Note> GenerateString(Note root, int frets)
+        {
+            var list = new List<Note>();
+            list.Add(root);
+            var last=root;
+            for(int i=1;i<frets+1;i++)
+            {
+                last=Halfstep(last);
+                list.Add(last);
+            }
+            return list;
+        }
+
+
+
     }
 }
