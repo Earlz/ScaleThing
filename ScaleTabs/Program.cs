74f8d85e32bc344e9c8fcfd087225086cad19b47
Jordan Earls <earlz@earlz.net>
2015-06-10 21:39:50 -0400

    basic scales etc

--- ScaleTabs/Program.cs
+++ ScaleTabs/Program.cs
@@ -8,8 +8,109 @@ namespace ScaleTabs
 {
     class Program
     {
-        static void Main(string[] args)
+        enum Note
         {
+            A = 0,
+            Asharp=1,
+            B=2,
+            C=3,
+            Csharp=4,
+            D=5,
+            Dsharp=6,
+            E=7,
+            F=8,
+            Fsharp=9,
+            G=10,
+            Gsharp=11
+        }
+
+        int MajorScale(Note root,Note n)
+        {
+            var ni = (int)n;
+            var rooti = (int)root;
+            var list = new List<Note>();
+
+            list.Add(root);
+            var tmp = Wholestep(root);
+            list.Add(tmp);
+            tmp = Wholestep(tmp);
+            list.Add(tmp);
+            tmp = Halfstep(tmp);
+            list.Add(tmp);
+            tmp = Wholestep(tmp);
+            list.Add(tmp);
+            tmp = Wholestep(tmp);
+            list.Add(tmp);
+            tmp = Wholestep(tmp);
+            list.Add(tmp);
+            //tmp = Halfstep(tmp);
+            //list.Add(tmp);
+            if(list.Contains(n))
+            {
+                return list.IndexOf(n);
+            }
+            return -1;
+        }
+
+        int MinorScale(Note root, Note n)
+        {
+            var ni = (int)n;
+            var rooti = (int)root;
+            var list = new List<Note>();
+
+            list.Add(root);
+            var tmp = Wholestep(root);
+            list.Add(tmp);
+            tmp = Halfstep(tmp);
+            list.Add(tmp);
+            tmp = Wholestep(tmp);
+            list.Add(tmp);
+            tmp = Wholestep(tmp);
+            list.Add(tmp);
+            tmp = Halfstep(tmp);
+            list.Add(tmp);
+            tmp = Wholestep(tmp);
+            list.Add(tmp);
+            //tmp = Wholestep(tmp); //octave
+            //list.Add(tmp);
+            if (list.Contains(n))
+            {
+                return list.IndexOf(n);
+            }
+            return -1;
+        }
+
+        Note Halfstep(Note n)
+        {
+            var ni = (int)n;
+            ni++;
+            if(ni == 12)
+            {
+                return Note.A;
+            }
+            return (Note)ni;
+        }
+        Note Wholestep(Note n)
+        {
+            var ni = (int)n;
+            ni++;
+            ni++;
+            if(ni == 12)
+            {
+                return Note.A;
+            }
+            if(ni == 13)
+            {
+                return Note.Asharp;
+            }
+            return (Note)ni;
+        }
+
+
+        void Main(string[] args)
+        {
+            int fretmax = 22;
+
         }
     }
 }
